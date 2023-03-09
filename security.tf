a9e91abc4f19231cbf73f49b3a35e94fac654f88
tulasiveera <115130908+tulasiveera@users.noreply.github.com>
2023-03-09 12:36:52 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,11 @@
+resource "aws_security_group" "sg" {
+  name        = "sg1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+   }
