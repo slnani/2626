8ffd8cdcd8316b6b28ac84f2347dd09d6382edb5
tulasiveera <115130908+tulasiveera@users.noreply.github.com>
2023-03-11 09:30:46 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,1 +1,26 @@
+esource "aws_security_group" "sg" {
+  name        = "sg1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name ="sg1"
+  }
+}
