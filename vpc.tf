d77fd9367211abf460b6b78a0391a3be16167701
tulasiveera <115130908+tulasiveera@users.noreply.github.com>
2023-03-11 09:10:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "lak" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
     tags = {
-    Name = "main"
+    Name = "lak"
     Purpose = "jenkins"
   }
 }
